c0774a2424899bb7327e7bdc607c8940b844926a
minusing <1776875119@qq.com>
2022-08-20 18:13:37 +0800

    修改后端url

--- src/network/http.ts
+++ src/network/http.ts
@@ -1,7 +1,7 @@
 //创建一个axios实例
 import request from "./request"
 const http = new request({
-    baseURL: 'http://haixtx.club:4000/api',
+    baseURL: 'http://haixtx.club:3000/api',
     timeout: 10000
 })
 export default http;
