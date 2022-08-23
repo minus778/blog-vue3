5489413469a2498851c008a544048d83f461363f
minusing <1776875119@qq.com>
2022-08-23 18:41:33 +0800

    修改后端htpps

--- src/network/http.ts
+++ src/network/http.ts
@@ -1,7 +1,7 @@
 //创建一个axios实例
 import request from "./request"
 const http = new request({
-    baseURL: 'http://haixtx.club:3000/api',
+    baseURL: 'https://node.haixtx.club/api',
     timeout: 10000
 })
 export default http;
