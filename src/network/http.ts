f967b92ee489c5c2e79d3b979aa004db615d696a
何武骏 <hewujun@supconit.com>
2024-01-23 14:41:12 +0800

    fix:修改请求代理

--- src/network/http.ts
+++ src/network/http.ts
@@ -1,7 +1,7 @@
 //创建一个axios实例
 import request from "./request"
 const http = new request({
-  baseURL: 'http://114.55.75.3:3000/api',
+  baseURL: '/api',
   // baseURL: 'http://localhost:4000/api',
   timeout: 200000
 })
