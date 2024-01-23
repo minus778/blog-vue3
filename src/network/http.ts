0ba868db7b4ec4a42406b0b34ab5780c13f4a69d
何武骏 <hewujun@supconit.com>
2024-01-23 14:17:45 +0800

    fix:修改nginx测试

--- src/network/http.ts
+++ src/network/http.ts
@@ -1,8 +1,8 @@
 //创建一个axios实例
 import request from "./request"
 const http = new request({
-    //baseURL: 'https://node.haixtx.club/api',
-    baseURL: 'http://localhost:4000/api',
-    timeout: 20000
+  baseURL: 'http://114.55.75.3:3000/api',
+  // baseURL: 'http://localhost:4000/api',
+  timeout: 200000
 })
 export default http;
